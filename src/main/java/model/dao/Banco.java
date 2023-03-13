cdb76735b72dee9dcee63ae3a47df39d483c6fee
raquel.andrade <raquel.andrade@FLE207D31.mshome.net>
2023-03-13 08:30:06 -0300

    Criando espaço de desenvolvimento.

--- src/main/java/model/dao/Banco.java
+++ src/main/java/model/dao/Banco.java
@@ -9,8 +9,7 @@ import java.sql.Statement;
 
 public class Banco {
 	
-	//private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; //MYSQL versão 8
-	private static final String DRIVER = "com.mysql.jdbc.Driver"; //MYSQL versão 5
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; //MYSQL versão 8
 	private static final String BANCODADOS = "dbfoodtruck";
 	private static final String CONEXAO = "jdbc:mysql://localhost:3306/" + BANCODADOS;
 	private static final String USER = "root";
